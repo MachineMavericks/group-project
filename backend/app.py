e7acf18154492349378de02c037c235fd90b1351
Guillaume-Barrier <guillaumbarrier@gmail.com>
2023-04-06 19:06:05 +0200

    Choice of the dataset
    /choice
    
    choose_dataset() in app.py
    ->saved in static/input/uploads
    
    dataset_selection.html
    =base of the selection, two first datasets and upload corner
    
    upload_done.html
    =change of layout, three datasets to choose from, links to /default
    
    upload_not_valid.html
    =/choice + error message

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 # DEFAULT IMPORTS:
 import os                                                   # OS
 # FLASK IMPORTS:
-from flask import Flask, render_template, request           # FLASK
+from flask import Flask, render_template, request, flash    # FLASK
+from werkzeug.utils import secure_filename
 from flask_restx import Api                                 # REST-X API
 # MODELS=
 from src.Models.Graph import Graph                          # GRAPH
@@ -15,12 +16,18 @@ input_dir = "static/input/"
 output_dir = "static/output/"
 os.mkdir(output_dir) and print("Can't find output directory. Creating one now.") \
     if not os.path.isdir(output_dir) else print("Found existing output directory.")
+UPLOAD_FOLDER = 'static/input/uploads'
 
 # GRAPH/NXGRAPH OBJECTS CONSTRUCTION -> SAVE TO PICKLE:
 graph = Graph(filepath=input_dir+"railway.csv", output_dir=output_dir, save_csvs=True, save_pickle=True)
 
+# FILE UPLOAD EXTENSIONS
+ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'tsv', 'ods'}
+
 # FLASK APP:
 app = Flask(__name__)
+# UPLOAD FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # BLUEPRINTS:
 app.register_blueprint(nxgraph_bp)
 
@@ -40,6 +47,26 @@ api.add_namespace(nxgraph_ns)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template("base.html")
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/choice', methods=['GET', 'POST'])
+def choose_dataset():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template("dataset/upload_not_valid.html")
+        file = request.files['file']
+        # If the user does not select a file, the page is refreshed.
+        if file.filename == '':
+            return render_template("dataset/upload_not_valid.html")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("dataset/upload_done.html")
+    return render_template("dataset/dataset_selection.html")
 
 def main():
     # START THE APP:
